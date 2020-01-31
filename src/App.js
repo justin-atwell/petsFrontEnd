6770c9f054ed7a1fa14b24f9cff2bde6d7813067
PaulBollinger <paul.bollinger@daugherty.com>
2020-01-31 14:48:34 -0600

    added a button that pops up a modal to pick a doctor

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import NewAppointment from './components/NewAppointment/NewAppointment.component'
-import NewAppointmentModal from './components/NewAppointment/NewAppointmentModal/NewAppointmentModal.component'
 import './App.css';
 
 function App() {
