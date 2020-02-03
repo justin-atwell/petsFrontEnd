09b83bec6d26ac172f8cc7c6f6a274ec9c46e543
PaulBollinger <paul.bollinger@daugherty.com>
2020-02-03 07:24:36 -0600

    added a blank option to the dropdowns

--- src/components/NewAppointment/NewAppointment.component.js
+++ src/components/NewAppointment/NewAppointment.component.js
@@ -23,14 +23,16 @@ const handleShow = () => { setShow (true);}
                                                 <div>
                                                       <label>Choose a Vet </label>
                                                       <select name="Vet">
+                                                            <option></option>
                                                             <option value="Dr. Funny Guy">Dr. Funny Guy</option>
                                                             <option value="Dr. Kevorkian">Dr. Kevorkian</option>
                                                             <option value="Dr. Smart Guy">Dr. Smart Guy</option>
                                                       </select>
                                                 </div>
                                                 <div>
-                                                      <label>Choose a Service Type</label>
+                                                      <label>Choose a Service Type </label>
                                                       <select>
+                                                            <option></option>
                                                             <option value="Grooming">Grooming</option>
                                                             <option value="Health Services">Health Services</option>
                                                             <option value="Pet Training">Pet Training</option>
