edb36a0fc659d5753847c669ac0e700409170c02
PaulBollinger <paul.bollinger@daugherty.com>
2020-02-03 07:13:31 -0600

    added Service Type dropdown

--- src/components/NewAppointment/NewAppointment.component.js
+++ src/components/NewAppointment/NewAppointment.component.js
@@ -23,15 +23,17 @@ const handleShow = () => { setShow (true);}
                                                 <div>
                                                       <label>Choose a Vet </label>
                                                       <select name="Vet">
-                                                            <option value="Dr. Big Nose">Dr. Big Nose</option>
+                                                            <option value="Dr. Funny Guy">Dr. Funny Guy</option>
                                                             <option value="Dr. Kevorkian">Dr. Kevorkian</option>
-                                                            <option value="Dr. Old Guy">Dr. Old Guy</option>
+                                                            <option value="Dr. Smart Guy">Dr. Smart Guy</option>
                                                       </select>
                                                 </div>
                                                 <div>
                                                       <label>Choose a Service Type</label>
                                                       <select>
-                                                            <option></option>
+                                                            <option value="Grooming">Grooming</option>
+                                                            <option value="Health Services">Health Services</option>
+                                                            <option value="Pet Training">Pet Training</option>
                                                       </select>
                                                 </div>
                                           </div>
