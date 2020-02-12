5c897fb2e2f0df3dac4d2ae8e0981f920e79f014
McKim <AAM1022@daugherty.com>
2020-02-12 16:31:19 -0600

    adding logic to dog breed comp to get array of breeds

--- src/components/dog.breed.component.js
+++ src/components/dog.breed.component.js
@@ -1,12 +1,37 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
+import DogBreedService from '../api/dog.breed.service';
 
-function DogBreeds() {
 
-const getDogBreeds = () => {
-    
-}
+
+
+
+const DogBreeds = (props) => {
+
+    //const [breeds, setBreeds] = useState({});
+    const [breeds, setBreeds] = useState([]);
+
+    const getDogBreeds = () => {
+        
+        DogBreedService.getAllBreeds()
+        .then(res => {
+            //console.log(res);
+            console.log(Object.keys(res.data.message));
+            //console.log(Object.values(res.data.message));
+            //const start = Object.keys(res.data.message);
+            setBreeds(Object.keys(res.data.message));
+            console.log(breeds);
+        })
+        .catch((error => {
+            console.log(error);
+        }));
+    }
     return <div>
-        <button>Get Dog Breeds</button>
+        <button onClick={getDogBreeds}>Get Dog Breeds</button>
+        
+        {/* <p type="text" value={breeds}></p>  */}
+        <div>
+
+        </div>
     </div>
 }
 
